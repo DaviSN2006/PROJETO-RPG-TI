7863dd6c313778e2aac540dee94bf4d1ded23e07
Delanes <danieldelanes@gmail.com>
2025-06-15 04:25:27 -0300

    Pequena implementação
    kk

--- PROJETO-RPG-TI/Assets/NPC.cs
+++ PROJETO-RPG-TI/Assets/NPC.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class NPC : MonoBehaviour, IInteractable
 {
     public string npcName = "Guardião";
+    public string npcPhrase = "";
     
 
     public void Interact()
     {
-        Debug.Log($"Olá, eu sou {npcName}. Bem-vindo ao nosso mundo!");
+        Debug.Log($"Interagindo com {npcName}: {npcPhrase}");
         // Aqui você pode iniciar um diálogo ou outra lógica
     }
 
